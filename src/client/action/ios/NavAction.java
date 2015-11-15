c49eccf8bb9a10c9cccea383e32baeb490859724
RexFun <evanole@163.com>
2015-11-15 16:14:13 +0800

    test

---
+++ src/client/action/ios/NavAction.java
@@ -0,0 +1,46 @@
+package client.action.ios;
+
+import java.util.Map;
+
+import gwen.devwork.BaseAction;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import client.entity.PaperCategory;
+import client.service.PaperNavService;
+
+@SuppressWarnings("serial")
+@Controller
+@Scope("prototype")
+@ParentPackage(value="struts-default")
+@Namespace("/client/ios")
+public class NavAction extends BaseAction<PaperCategory>
+{
+	@Autowired
+	private PaperNavService paperNavService;
+
+	@Action(value="getNav1Json")
+	public void getNav1Json()
+	{
+		getBaseResp().setContentType("application/json");
+		String result = paperNavService.getNav1Json();
+		System.out.println(result);
+		print(result);
+	}
+	
+	@Action(value="getNav2Json")
+	public void getNav2Json()
+	{
+		getBaseResp().setContentType("application/json");
+		Map m = getReq().getParameterValueMap(false, true);
+		String result = paperNavService.getNav2Json(m);
+		System.out.println(result);
+		print(result);
+	}
+	
+}
---
+++ src/client/service/PaperNavService.java
@@ -0,0 +1,66 @@
+package client.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import gwen.devwork.BaseDao;
+import gwen.devwork.BaseService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+
+import client.dao.PaperCategoryDao;
+import client.dao.PaperModelDao;
+import client.entity.PaperCategory;
+import client.entity.PaperModel;
+
+@Service("paperNavService")
+public class PaperNavService extends BaseService<PaperCategory,Long>
+{
+	@Autowired
+	private PaperCategoryDao paperCategoryDao;
+	@Autowired
+	private PaperModelDao paperModelDao;
+
+	@Override
+	public BaseDao<PaperCategory,Long> getEntityDao() {
+		return paperCategoryDao;
+	}
+	
+	public String getNavJson()
+	{
+		Map m = new HashMap();
+		List<PaperCategory> pcList = paperCategoryDao.get(m);
+		List<PaperModel> pmList = paperModelDao.get(m);
+		for(PaperCategory pc : pcList)
+		{
+			for(PaperModel pm : pmList)
+			{
+				if(pc.getId() == pm.getPid())
+				{
+					pc.addPaperModel(pm);
+				}
+			}
+		}
+		Gson gson = new Gson();  
+		return gson.toJson(pcList);
+	}
+	
+	public String getNav1Json()
+	{
+		Map m = new HashMap();
+		List<PaperCategory> pcList = paperCategoryDao.get(m);
+		Gson gson = new Gson();  
+		return gson.toJson(pcList);
+	}
+	
+	public String getNav2Json(Map m)
+	{
+		List<PaperModel> pcList = paperModelDao.get(m);
+		Gson gson = new Gson();  
+		return gson.toJson(pcList);
+	}
+}
